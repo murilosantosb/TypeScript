881557590e26fe7e71352f0daf77e20b383b156f
Murilo <142051797+murilosantosb@users.noreply.github.com>
2024-05-21 13:11:10 -0300

    .

--- 6_OBJECT_TYPES/index.js
+++ 6_OBJECT_TYPES/index.js
@@ -24,4 +24,37 @@ const fusca = {
     wheels: 4
 };
 console.log(fusca);
-// fusca.wheels = 5
+let coords = {
+    x: 10
+};
+coords.y = 15;
+console.log(coords);
+const murilo = {
+    name: "Murilo",
+    age: 17
+};
+const bob = {
+    name: "Bob",
+    age: 9,
+    superpowers: ["Esfomeado", "Conquistador", "Ladrãozinho"]
+};
+console.log(murilo);
+console.log(bob);
+const arnold = {
+    name: "Arnold",
+    type: "Shotgun",
+    caliber: 12
+};
+console.log(arnold);
+// 7 - Readonly array
+let myArray = ["Maçã", "Laranja", "banana"];
+// myArray[3] = "Mamão"
+console.log(myArray);
+myArray.forEach((item, index) => {
+    console.log(`Fruta ${index} = ${item}`);
+});
+myArray = myArray.map((item) => {
+    return `Fruta: ${item}`;
+});
+console.log(myArray);
+const myNumberArray = [1, 2, 3, "4", 5];
--- 6_OBJECT_TYPES/index.ts
+++ 6_OBJECT_TYPES/index.ts
@@ -47,3 +47,82 @@ const fusca: Car = {
 console.log(fusca)
 
 // fusca.wheels = 5
+
+// 4 - Index signature
+interface CoordObject {
+    [index: string]: number
+}
+
+let coords: CoordObject = {
+    x: 10
+}
+
+coords.y = 15
+
+console.log(coords)
+
+// 5 - Extending interfaces
+interface Human {
+    name: string
+    age: number
+}
+
+interface SuperHuman extends Human {
+    superpowers: string[]
+} 
+
+const murilo: Human = {
+    name: "Murilo",
+    age: 17
+}
+
+const bob: SuperHuman = {
+    name: "Bob",
+    age: 9,
+    superpowers: ["Esfomeado", "Conquistador","Ladrãozinho"]
+}
+
+console.log(murilo)
+console.log(bob)
+
+// 6 - Intersection types
+interface Character {
+    name: string   
+}
+
+interface Gun {
+    type: string
+    caliber: number
+}
+
+type HumanWithGun = Character & Gun
+
+const arnold: HumanWithGun = {
+    name: "Arnold",
+    type: "Shotgun",
+    caliber: 12
+}
+
+console.log(arnold)
+
+// 7 - Readonly array
+let myArray: ReadonlyArray<string> = ["Maçã", "Laranja", "banana"]
+
+// myArray[3] = "Mamão"
+
+console.log(myArray)
+
+myArray.forEach((item, index) => {
+    console.log(`Fruta ${index} = ${item}`)
+})
+
+myArray = myArray.map((item) => {
+    return `Fruta: ${item}`
+})
+
+console.log(myArray)
+
+// 8 - Tuplas
+type fiveNumbers = [number, number, number, string, number]
+
+const myNumberArray: fiveNumbers = [1,2,3,"4",5]
